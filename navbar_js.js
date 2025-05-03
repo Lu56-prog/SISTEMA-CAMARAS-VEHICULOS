c87c1657e956a2a31d1d8d900ba5643e1359ceeb
Lu56-prog <lucerocardona167@gmail.com>
2025-05-03 17:40:52 -0500

    registro de vehiculo

---
+++ navbar_js.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("navbar.html")  
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("nav-principal").innerHTML = data;
+            const navbar = document.querySelector(".navbar");
+            window.addEventListener("scroll", () => {
+                if (window.scrollY > 80) {
+                    navbar.classList.add("fixed");
+                } else {
+                    navbar.classList.remove("fixed");
+                }
+            });
+        })
+        .catch(err => console.error('Error cargando el navbar:', err));
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+});
+
+  
