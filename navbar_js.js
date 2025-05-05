217cefdb9e62040d389f26199419c08df3a7bd56
Lu56-prog <lucerocardona167@gmail.com>
2025-05-05 10:53:45 -0500

    PAGINA PRINCIPAL

--- navbar_js.js
+++ navbar_js.js
@@ -2,6 +2,24 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch("navbar.html")  
         .then(response => response.text())
         .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+            const navbar = document.querySelector(".navbar");
+            window.addEventListener("scroll", () => {
+                if (window.scrollY > 80) {
+                    navbar.classList.add("fixed");
+                } else {
+                    navbar.classList.remove("fixed");
+                }
+            });
+        })
+        .catch(err => console.error('Error cargando el navbar:', err));
+  });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("navbar_principal.html")  
+        .then(response => response.text())
+        .then(data => {
             document.getElementById("nav-principal").innerHTML = data;
             const navbar = document.querySelector(".navbar");
             window.addEventListener("scroll", () => {
